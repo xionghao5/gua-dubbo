9887500730fdb26fb94cd5d5bd3ee214565715dd
xionghao <18829037917@163.com>
2024-03-24 15:38:14 +0800

    新增一个用户类

---
+++ dubbo-consumer/src/main/java/com/gua/dubbo/consumer/mongodb/User.java
@@ -0,0 +1,31 @@
+package com.gua.dubbo.consumer.mongodb;
+
+public class User {
+    private String username;
+    private Integer age;
+    private Integer gender;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+}
